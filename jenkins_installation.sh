d6c58c956d8fe910059f43a92f036df9746a18a4
roshan-chandekar <84260415+roshan-chandekar@users.noreply.github.com>
2022-02-27 08:44:02 +0530

    Update jenkins_installation.sh

--- jenkins_installation.sh
+++ jenkins_installation.sh
@@ -6,6 +6,6 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-apt install default-jre -y
+sudo apt install openjdk-8-jdk -y
 sudo apt-get install jenkins -y
 sudo systemctl status jenkins
